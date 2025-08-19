9e5461e6b0a6599c742cae118c5b43043bc0eb96
Quintex9 <misko.smatana@gmail.com>
2025-08-19 15:59:46 +0200

    Definovanie entity Category

--- src/main/java/com/destination/destinations/model/Category.java
+++ src/main/java/com/destination/destinations/model/Category.java
@@ -1,4 +1,18 @@
 package com.destination.destinations.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Set;
+
+@Entity
+@Data
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<Destination> destinations;
 }
