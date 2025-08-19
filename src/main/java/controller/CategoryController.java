ce1bb155b2ac989ac9e3fd649bdf5f9d2c87afdc
Quintex9 <misko.smatana@gmail.com>
2025-08-19 17:59:35 +0200

    Pridanie CategoryRepository

--- src/main/java/controller/CategoryController.java
+++ src/main/java/controller/CategoryController.java
@@ -1,4 +1,16 @@
 package controller;
 
+import model.Category;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/categories")
 public class CategoryController {
+
+    public List<Category> getAllCategories() {
+
+    }
 }
