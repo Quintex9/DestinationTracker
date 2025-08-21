a93b03cacd44c1f3b3b814634bad6429383cfad3
Quintex9 <misko.smatana@gmail.com>
2025-08-21 16:11:52 +0200

    Vytvorenie, nastavenie DestinationList.jsx, úprava app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
+import DestinationList from "./assets/DestinationList";
 
 function App() {
   const [destinations, setDestinations] = useState([]);
 
-  
+  useEffect(() => {
+    fetch("http://localhost:8080/api/destinations")
+    .then((response) =>response.json())
+    .then(data => setDestinations(data));
+  }, []);
 
-  return (
-    <div>
-      
+  return(
+    <div className="container">
+      <div>
+        {destinations.length ? (
+          <DestinationList destinations={destinations}/>
+        ) : (
+          <p>No products found</p>
+        )}
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ frontend/src/assets/DestinationList.jsx
@@ -0,0 +1,26 @@
+const DestinationList = ({ destinations = [] }) => {
+  return (
+    <div className="row">
+      {destinations.map((destination) => (
+        <div className="col-lg-4 col-md-6 col-sm-12 mb-4" key={destination.id}>
+          <div className="card h-100">
+            <img
+              src={destination.imageUrl || "https://placehold.co/600x400"}
+              className="card-img-top"
+              alt={destination.name}
+            />
+            <div class="card-body">
+                <h5 class="card-title">{destination.name}</h5>
+                <p class="card-text">{destination.description}</p>
+                {destination.price > 0.0 ? (<p class="card-text">{destination.price}</p>)
+                :
+                undefined}
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default DestinationList;
