7bf9c4f619dd010acfeceab8738dace25d2adda5
Quintex9 <misko.smatana@gmail.com>
2025-08-21 13:53:15 +0200

    Vytvorenie frontendu

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [destinations, setDestinations] = useState([]);
+
+  
+
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default App;
