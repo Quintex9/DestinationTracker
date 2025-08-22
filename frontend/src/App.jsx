3c94730c34e8a27f54bb6a5b2af1baac0d6cea88
Quintex9 <misko.smatana@gmail.com>
2025-08-22 12:54:19 +0200

    Pridanie funkcionality triedenia + searchBar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,6 @@ function App() {
 
   const [selectedCategory, setSelectedCategory] = useState(null);
   const [searchTerm, setSearchTerm] = useState("");
-  const [sortOrder, setSortOrder] = useState("asc");
 
   useEffect(() => {
     fetch("http://localhost:8080/api/destinations")
@@ -25,13 +24,18 @@ function App() {
     setSearchTerm(event.target.value);
   };
 
-  const handleSortChange = (event) => {
-    setSortOrder(event.target.value);
-  };
-
   const handleCategorySelect = (categoryId) => {
     setSelectedCategory(categoryId ? Number(categoryId) : null);
   };
+
+  const filteredDestinations = (
+  selectedCategory
+    ? categories.find(c => c.id === selectedCategory)?.destinations || []
+    : destinations
+).filter(destination =>
+  destination.name.toLowerCase().includes(searchTerm.toLowerCase())
+);
+
 
   return (
     <div className="container">
@@ -39,7 +43,10 @@ function App() {
         <h1 className="my-4">Naše spoločné výlety</h1>
         <div className="row allign-items-center mb-4">
           <div className="col-md-3 col-sm-12 mb-12">
-            <CategoryFilter categories={categories} onSelect={handleCategorySelect}/>
+            <CategoryFilter
+              categories={categories}
+              onSelect={handleCategorySelect}
+            />
           </div>
           <div className="col-md-5 col-sm 12 mb-12">
             <input
@@ -49,15 +56,9 @@ function App() {
               onChange={handleSearchChange}
             ></input>
           </div>
-          <div className="col-md-4 col-sm-12 mb-2">
-            <select className="form-control" onChange={handleSortChange}>
-              <option value="asc">Sort by Price: Low to High</option>
-              <option value="desc">Sort by Price: High to Low</option>
-            </select>
-          </div>
         </div>
-        {destinations.length ? (
-          <DestinationList destinations={destinations} />
+        {filteredDestinations.length ? (
+          <DestinationList destinations={filteredDestinations} />
         ) : (
           <p>No products found</p>
         )}
