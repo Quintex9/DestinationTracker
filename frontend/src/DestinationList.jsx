da7c4cfd1ea44ca5375ebfbada11141860649931
Quintex9 <misko.smatana@gmail.com>
2025-08-22 17:16:39 +0200

    Pridanie css úprav

--- frontend/src/DestinationList.jsx
+++ frontend/src/DestinationList.jsx
@@ -3,16 +3,16 @@ const DestinationList = ({ destinations = [] }) => {
     <div className="row">
       {destinations.map((destination) => (
         <div className="col-lg-4 col-md-6 col-sm-12 mb-4" key={destination.id}>
-          <div className="card h-100">
+          <div className="card h-100 text-primary-emphasis bg-primary-subtle border border-primary-subtle rounded-3">
             <img
-              src={destination.imageUrl || "https://placehold.co/600x400"}
-              className="card-img-top"
+              src={destination.imageURL || "https://placehold.co/600x400"}
+              className="card-img-top fixed-image"
               alt={destination.name}
             />
             <div className="card-body">
                 <h5 className="card-title">{destination.name}</h5>
                 <p className="card-text">{destination.description}</p>
-                {destination.price > 0.0 ? (<p class="card-text">{destination.price}</p>)
+                {destination.price > 0.0 ? (<p class="card-text">{destination.price}€</p>)
                 :
                 undefined}
             </div>
