827dbcb74b7a1edc69b8307d415e89f87c1c3cfe
Quintex9 <misko.smatana@gmail.com>
2025-09-01 16:22:11 +0200

    Pridanie finálnych komentárov a hodnotení pre terajšie destinácie

--- src/main/java/com/destination/destinations/config/DataSeeder.java
+++ src/main/java/com/destination/destinations/config/DataSeeder.java
@@ -49,12 +49,13 @@ public class DataSeeder implements CommandLineRunner {
         kupeleMaleBielice.setPrice(24.0);
         kupeleMaleBielice.setCategory(spa);
         kupeleMaleBielice.setDuration(4.0);
-        kupeleMaleBielice.setNazorLenka("Lenkin názor");
-        kupeleMaleBielice.setNazorMichal("Michalov názor");
+        kupeleMaleBielice.setNazorLenka("Vonku bolo hrozne veľa ľudí a to mi strašne prekážalo. Ale vnútri bolo len pár starších a my čiže super relax. Nemám moc rada verejné priestory s vodou ale celkom sme zrelaxovali.\n" +
+                "Body strhávam za to že nebol hot dog v bufete a spravili mi extrém masný langoš.\n");
+        kupeleMaleBielice.setNazorMichal("Celkom dobrý výlet. Nebolo veľa bazénov ani dobré sauny ale vnútri nebolo moc ľudí a bola príjemne teplá voda. Jedlo z bufetu nebolo najlepšie ale chutilo.");
         kupeleMaleBielice.setMichalURL("/images/Michal.jpeg");
         kupeleMaleBielice.setLenkaURL("/images/Lenka.png");
-        kupeleMaleBielice.setRatingLenka(3.5);
-        kupeleMaleBielice.setRatingMichal(4.5);
+        kupeleMaleBielice.setRatingLenka(3.0);
+        kupeleMaleBielice.setRatingMichal(3.0);
         kupeleMaleBielice.setFarba("blue-card");
 
         Destination javorina = new Destination();
@@ -64,12 +65,12 @@ public class DataSeeder implements CommandLineRunner {
         javorina.setPrice(0.0);
         javorina.setCategory(hiking);
         javorina.setDuration(2.0);
-        javorina.setNazorLenka("Lenkin názor");
-        javorina.setNazorMichal("Michalov názor");
+        javorina.setNazorLenka("Nikdy viac. Horšiu svalobicu som nemala. Krajina bola nádherná to musím uznať. Aj výhľad. Ale hrooozne dlhé to bolo, nevládala som, potila som sa, muchy ma žrali. \n");
+        javorina.setNazorMichal("Nebola to síce najľahšia túra ale bolo pekne vonku a bola to taká výzva. Pravdepodobne by som ešte raz zopakoval ale len na bicykli.");
         javorina.setMichalURL("/images/Michal.jpeg");
         javorina.setLenkaURL("/images/Lenka.png");
-        javorina.setRatingLenka(3.5);
-        javorina.setRatingMichal(4.5);
+        javorina.setRatingLenka(2.0);
+        javorina.setRatingMichal(3.0);
         javorina.setFarba("green-card");
 
         Destination marhat = new Destination();
@@ -79,11 +80,11 @@ public class DataSeeder implements CommandLineRunner {
         marhat.setPrice(0.0);
         marhat.setCategory(hiking);
         marhat.setDuration(2.0);
-        marhat.setNazorLenka("Túra na Marhát bola nádherná! Nebola príliš ťažká, takže som si ju užila bez toho, aby som bola úplne vyčerpaná. Cestou hore krásna príroda a na vrchole úžasný kľud a výhľady – ideálne na oddych aj romantické chvíle. Pre mňa to bol skvelý deň v prírode");
-        marhat.setNazorMichal("Výstup na Marhát bol fakt super! Trasa bola nenáročná, ale pritom som sa aj zapotil, takže som mal pocit, že som si to odmakal. Hore je výhľad, ktorý stojí za to – ideálne miesto na oddych s kamošmi a dobré fotky. Pre mňa taká túra, čo spojí pohodu s troškou športu");
+        marhat.setNazorLenka("Bomba turistika. Nádhera. Krása. Úžas. Zopakobala by som. Dokonca som aj dokázala vyliezť na rozhladňu. 0.1 boda strhávam lebo dali eknihu na miesto kde nejdú dáta a za to ako Adelka a Maco nadávali na turistiku.");
+        marhat.setNazorMichal("Pokojná túra, nebola moc ťažká. Keby ideme samy tak to zvládneme aj do 2 hodín. Krásny výhľad po ceste aj z rozhliadne, no bola trochu rozheganá.");
         marhat.setMichalURL("/images/Michal.jpeg");
         marhat.setLenkaURL("/images/Lenka.png");
-        marhat.setRatingLenka(3.5);
+        marhat.setRatingLenka(4.9);
         marhat.setRatingMichal(4.5);
         marhat.setFarba("green-card");
 
@@ -94,12 +95,12 @@ public class DataSeeder implements CommandLineRunner {
         gric.setPrice(0.0);
         gric.setCategory(hiking);
         gric.setDuration(2.0);
-        gric.setNazorLenka("Lenkin názor");
-        gric.setNazorMichal("Michalov názor");
+        gric.setNazorLenka("Naša prvá turistika vo výzve. Bolo to vskutku pekné. Body dávam za výhľad na Hnadlovú a za to že som si zobrala pivko a na vrchole vypila ale výstup ma skoro zabil. Skajka utekala jak teren koza a ja som nestíhala. Plus skoro sme sa stratili v dedine keď sme sa vracali. A časť cesty výstupu bola zarastená jak džungla. Čudujem sa že som kliešťa nemala až v riti.\n");
+        gric.setNazorMichal("Zatiaľ moja najmenej obľúbená túra. Nebola nejaká záživná cesta. Chodili sme cez zarastenú trávu, aspoň výhľad bol pekný.");
         gric.setMichalURL("/images/Michal.jpeg");
         gric.setLenkaURL("/images/Lenka.png");
-        gric.setRatingLenka(3.5);
-        gric.setRatingMichal(4.5);
+        gric.setRatingLenka(3.0);
+        gric.setRatingMichal(2.0);
         gric.setFarba("green-card");
 
         Destination inovec = new Destination();
@@ -109,12 +110,12 @@ public class DataSeeder implements CommandLineRunner {
         inovec.setPrice(0.0);
         inovec.setCategory(hiking);
         inovec.setDuration(2.0);
-        inovec.setNazorLenka("Lenkin názor");
-        inovec.setNazorMichal("Michalov názor");
+        inovec.setNazorLenka("Topka. Vyviezli sme sa až po chatu autom aj keď Miško asi nemal moc dobré pocity z toho. A za 40 min sme boli na inovci. Máme z tade pekné fotky a dobrú storku na ig som spravila. Plus sme našli hríby kde sme mali cikpauzu xd");
+        inovec.setNazorMichal("Nebolo zrovna veľa pekných výhľadov. Hore sme išli autom až pri chatu - dlho trvala cesta na aute. Výhľad veľmi pekný a konečne stabilná rozhliadňa.");
         inovec.setMichalURL("/images/Michal.jpeg");
         inovec.setLenkaURL("/images/Lenka.png");
-        inovec.setRatingLenka(3.5);
-        inovec.setRatingMichal(4.5);
+        inovec.setRatingLenka(5.0);
+        inovec.setRatingMichal(4.0);
         inovec.setFarba("green-card");
 
         Destination oponicky = new Destination();
@@ -122,29 +123,29 @@ public class DataSeeder implements CommandLineRunner {
         oponicky.setDescription("Ľahký výstup, krásny výhľad. Pohodlná prechádzka.");
         oponicky.setImageURL("/images/oponicky.jpg");
         oponicky.setPrice(0.0);
-        oponicky.setCategory(hiking);
+        oponicky.setCategory(castles);
         oponicky.setDuration(2.0);
-        oponicky.setNazorLenka("Lenkin názor");
-        oponicky.setNazorMichal("Michalov názor");
+        oponicky.setNazorLenka("Kiko nás zobral až do veže hradu, veľmi chillový spotík. Ale nebola som moc dobre oblečená a bolo hrozne teplo. Inak krásne šla by som zas. \n");
+        oponicky.setNazorMichal("Naša prvá \"túra\". Síce som tento hrad už predtým navštívil ale výhľad sa mi veľmi páčil. Dobre sa sedelo na hrade. Dobrý výlet s Kikom. ");
         oponicky.setMichalURL("/images/Michal.jpeg");
         oponicky.setLenkaURL("/images/Lenka.png");
-        oponicky.setRatingLenka(3.5);
-        oponicky.setRatingMichal(4.5);
+        oponicky.setRatingLenka(4.5);
+        oponicky.setRatingMichal(4.8);
         oponicky.setFarba("green-card");
 
         Destination ranc = new Destination();
         ranc.setName("Ranč pod Babicou");
         ranc.setDescription("Malá zoo na Bojnej. Krásne zvieratká, zopár z nich sme si mohli pohľadiť");
         ranc.setImageURL("/images/ranc.jpg");
-        ranc.setPrice(0.0);
+        ranc.setPrice(20.0);
         ranc.setCategory(Zoo);
         ranc.setDuration(2.0);
-        ranc.setNazorLenka("Lenkin názor");
-        ranc.setNazorMichal("Michalov názor");
+        ranc.setNazorLenka("Veľmi pekné, vystihli sme dobré počasie takže bolo príjemme. Pokŕmila som ary a lemury a Miško ma s nimi pofotil. Dobre sme sa najedli a neprišlo mi že to bolo také drahé. Pol boda strhávam za to že som nemohla kŕmiť surikaty.\n");
+        ranc.setNazorMichal("Vonku super počasie, nebolo moc draho. Veľa zvierat, páčilo sa mi, že sme mohli niektoré pohľadiť. Papagáje mi stále skákali na chrbát.");
         ranc.setMichalURL("/images/Michal.jpeg");
         ranc.setLenkaURL("/images/Lenka.png");
-        ranc.setRatingLenka(3.5);
-        ranc.setRatingMichal(4.5);
+        ranc.setRatingLenka(4.5);
+        ranc.setRatingMichal(5.0);
         ranc.setFarba("orange-card");
 
         destinationRepository.saveAll(Arrays.asList(marhat, gric, inovec,kupeleMaleBielice,javorina, oponicky, ranc));
