402c35a5340b03a28bf8ce03e7c7f0016e369f8d
Quintex9 <misko.smatana@gmail.com>
2025-09-02 14:23:20 +0200

    return v Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,8 +1,10 @@
 export default function Footer() {
-  <footer className="footer footer-center">
-    <div className="container">
+  return (
+    <footer className="footer footer-center">
+      <div className="container">
         <p>Copyright &copy; 2025. All rights reserved.</p>
         <p>Vyrobil: Michal Smatana</p>
-    </div>
-  </footer>;
+      </div>
+    </footer>
+  );
 }
